0db9da2c664b17758ae84208cbaad7756a390181
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2023-02-13 13:13:59 -0600

    Added selection and deselection events

---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,31 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Unit : NetworkBehaviour
+{
+    [SerializeField] UnityEvent onSelected = null;
+    [SerializeField] UnityEvent onDeselected = null;
+
+    #region Client
+
+    [Client]
+    public void Select()
+    {
+        if (!isOwned) { return; }
+
+        onSelected?.Invoke();
+    }
+
+    [Client]
+    public void Deselect()
+    {
+        if (!isOwned) { return; }
+
+        onDeselected?.Invoke();
+    }
+
+    #endregion
+}
