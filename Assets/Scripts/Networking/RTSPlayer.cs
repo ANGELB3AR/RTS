157106ad63d96a834bb4f931ba72848a6d77fe68
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2023-02-13 15:36:07 -0600

    Server now stores a list of units for each player

---
+++ Assets/Scripts/Networking/RTSPlayer.cs
@@ -0,0 +1,36 @@
+using Mirror;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RTSPlayer : NetworkBehaviour
+{
+    List<Unit> myUnits = new List<Unit>();
+
+    public override void OnStartServer()
+    {
+        Unit.ServerOnUnitSpawned += ServerHandleUnitSpawned;
+        Unit.ServerOnUnitDespawned += ServerHandleUnitDespawned;
+    }
+
+    public override void OnStopServer()
+    {
+        Unit.ServerOnUnitSpawned -= ServerHandleUnitSpawned;
+        Unit.ServerOnUnitDespawned -= ServerHandleUnitDespawned;
+    }
+
+    private void ServerHandleUnitSpawned(Unit unit)
+    {
+        if (unit.connectionToClient.connectionId != connectionToClient.connectionId) { return; }
+
+        myUnits.Add(unit);
+    }
+
+    private void ServerHandleUnitDespawned(Unit unit)
+    {
+        if (unit.connectionToClient.connectionId != connectionToClient.connectionId) { return; }
+
+        myUnits.Remove(unit);
+    }
+}
--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -1,4 +1,5 @@
 using Mirror;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,10 +11,27 @@ public class Unit : NetworkBehaviour
     [SerializeField] UnityEvent onSelected = null;
     [SerializeField] UnityEvent onDeselected = null;
 
+    public static event Action<Unit> ServerOnUnitSpawned;
+    public static event Action<Unit> ServerOnUnitDespawned;
+
     public UnitMovement GetUnitMovement()
     {
         return unitMovement;
     }
+
+    #region Server
+
+    public override void OnStartServer()
+    {
+        ServerOnUnitSpawned?.Invoke(this);
+    }
+
+    public override void OnStopServer()
+    {
+        ServerOnUnitDespawned?.Invoke(this);
+    }
+
+    #endregion
 
     #region Client
 
