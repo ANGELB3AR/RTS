7d4307e51df25129112689d6eaa8f5a730f2e511
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2023-02-15 06:54:45 -0600

    Tanks fire projectiles toward enemies

---
+++ Assets/Scripts/Units/UnitFiring.cs
@@ -0,0 +1,43 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitFiring : NetworkBehaviour
+{
+    [SerializeField] Targeter targeter = null;
+    [SerializeField] GameObject projectilePrefab = null;
+    [SerializeField] Transform projectileSpawnPoint = null;
+    [SerializeField] float fireRange = 5f;
+    [SerializeField] float fireRate = 1f;
+    [SerializeField] float rotationSpeed = 20f;
+
+    float lastFireTime;
+
+    [ServerCallback]
+    private void Update()
+    {
+        if (!CanFireAtTarget()) { return; }
+
+        Quaternion targetRotation = Quaternion.LookRotation(targeter.GetTarget().transform.position - transform.position);
+
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        if (Time.time > (1 / fireRate) + lastFireTime)
+        {
+            Quaternion projectileRotation = Quaternion.LookRotation(targeter.GetTarget().GetAimAtPoint().position - projectileSpawnPoint.position);
+
+            GameObject projectileInstance = Instantiate(projectilePrefab, projectileSpawnPoint.position, projectileRotation);
+
+            NetworkServer.Spawn(projectileInstance, connectionToClient);
+
+            lastFireTime = Time.time;
+        }
+    }
+
+    [Server]
+    bool CanFireAtTarget()
+    {
+        return Vector3.Distance(targeter.GetTarget().transform.position, transform.position) < fireRange;
+    }
+}
