f3da5e0460450fbde94a0e73ace39544a1722fa9
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2023-02-17 12:17:27 -0600

    Created events for base spawning and despawning

--- Assets/Scripts/Units/UnitBase.cs
+++ Assets/Scripts/Units/UnitBase.cs
@@ -1,24 +1,33 @@
 using Mirror;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UnitBase : NetworkBehaviour
 {
-    [SerializeField] Health health;
+    [SerializeField] Health health = null;
+
+    public static event Action<UnitBase> ServerOnBaseSpawned;
+    public static event Action<UnitBase> ServerOnBaseDespawned;
 
     #region Server
 
     public override void OnStartServer()
     {
+        ServerOnBaseSpawned?.Invoke(this);
+        
         health.ServerOnDie += ServerHandleDie;
     }
 
     public override void OnStopServer()
     {
+        ServerOnBaseDespawned?.Invoke(this);
+        
         health.ServerOnDie -= ServerHandleDie;
     }
 
+    [Server]
     void ServerHandleDie()
     {
         NetworkServer.Destroy(gameObject);
